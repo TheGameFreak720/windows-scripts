d4a3438069d79afedc6659e17ee641f7893854a6
Ori Alvarez <ori.alvarez@commithub.com>
2021-07-11 13:19:27 -0400

    added veracrypt

--- set-dev-env.bat
+++ set-dev-env.bat
@@ -1,1 +1,1 @@
-choco install nodejs yarn vim postman git cmder vscode gimp ntop.portable openssh drawio etcher -y
+choco install nodejs yarn vim postman git cmder vscode gimp ntop.portable openssh drawio etcher veracrypt -y
